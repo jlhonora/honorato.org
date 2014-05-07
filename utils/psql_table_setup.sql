1ae8798203b42c6efdd5632872d97d0485842f26
jlhonora <jlhonora@ing.puc.cl>
2014-05-07 19:03:41 -0400

    Working towards a long-term table

--- utils/psql_table_setup.sql
+++ utils/psql_table_setup.sql
@@ -8,10 +8,36 @@ CREATE TABLE activities
 	created_at		timestamp
 );
 
+CREATE TABLE nodes
+(
+	id				SERIAL PRIMARY KEY, 
+	name            text,
+	created_at		timestamp
+);
+
+CREATE TABLE configs
+(
+	id				SERIAL PRIMARY KEY,
+	node_id			integer REFERENCES nodes (node_id),
+	sensor_id		integer REFERENCES sensors (sensor_id),
+	formula			text,
+	created_at		timestamp
+);
+
+CREATE TYPE sensor_type AS ENUM ('default', 'counter', 'temp', 'hum');
+CREATE TABLE sensors
+(
+	id				SERIAL PRIMARY KEY,
+	type			sensor_type, 
+	name            text,
+	created_at		timestamp,
+	updated_at		timestamp
+);
+
 CREATE TABLE measurements
 (
 	id				SERIAL PRIMARY KEY, 
-	sensor_type		smallint, 
+	sensor_id		integer REFERENCES sensors (sensor_id),
 	value			real, 
 	created_at		timestamp
 );
